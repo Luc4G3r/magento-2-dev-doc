38414d2be2443bf355ad7c9b7a86d940f5be66d5
Luc4G3r <47355690+Luc4G3r@users.noreply.github.com>
2021-11-11 11:12:12 +0100

    Update deploy.sh

--- SAMPLES/deploy.sh
+++ SAMPLES/deploy.sh
@@ -1,38 +1,71 @@
 #!/usr/bin/env bash
 
+# Prepare environment vars
+SCRIPTPATH=$(dirname "$(realpath -- "$0")")
+if [ -f "$SCRIPTPATH"/.deploy_env ]
+then
+  export $(cat "$SCRIPTPATH"/.deploy_env | sed 's/#.*//g' | xargs)
+fi
+
+if [[ -z "${user}" ]]; then
+  echo "Please set user variable in .deploy_env"
+  exit
+fi
+
+if [[ -z "${group}" ]]; then
+  group=$user
+fi
+
+if [[ -z "${git_branch_name}" ]]; then
+  git_branch_name=master
+fi
+
+if [[ -z "${magento_root_subdir}" ]]; then
+  magento_root_subdir=httpdocs
+fi
+
+if [[ -z "${php_executable}" ]]; then
+  php_executable=php
+fi
+
+echo "$user:$group @$git_branch_name in $magento_root_subdir running $php_executable"
+
 # Sample deploy script
 
 echo 'set rights'
 
-php -v
+$php_executable -v
 
-chown -R user:group httpdocs/
-chmod -R 755 httpdocs/
-find . -type d -exec chmod 770 {} \;
-find . -type f -exec chmod 660 {} \;
-chmod u+x bin/magento
+chown -R $user:$group $magento_root_subdir
+chmod -R 755 $magento_root_subdir
+find ./$magento_root_subdir/ -type d -exec chmod 770 {} \;
+find ./$magento_root_subdir/ -type f -exec chmod 660 {} \;
+chmod u+x ./$magento_root_subdir/bin/magento
 
 echo '---------------------------------------------------------'
 
-php httpdocs/bin/magento maintenance:enable
+date=`date +"%Y-%m-%d"`
+printf "%s\n" $date | tee -a /$magento_root_subdir/var/log/git_deploy_history.log
 
-cd httpdocs/
-git pull
+$php_executable $magento_root_subdir/bin/magento maintenance:enable
 
-php composer install --no-cache --no-dev
+cd $magento_root_subdir
+git pull origin $git_branch_name >&1 | tee -a /$magento_root_subdir/var/log/git_deploy_history.log
+
+$php_executable composer install --no-cache --no-dev
 cd ..
 
 echo '---------------------------------------------------------'
 
-php httpdocs/bin/magento cache:flush
-php httpdocs/bin/magento setup:upgrade
-php httpdocs/bin/magento setup:di:compile
-php httpdocs/bin/magento setup:static-content:deploy de_DE en_US -f
-php httpdocs/bin/magento indexer:reindex
-php httpdocs/bin/magento maintenance:disable
+$php_executable $magento_root_subdir/bin/magento cache:flush
+$php_executable $magento_root_subdir/bin/magento setup:upgrade
+$php_executable $magento_root_subdir/bin/magento setup:di:compile
+$php_executable $magento_root_subdir/bin/magento setup:static-content:deploy de_DE en_US -f
+$php_executable $magento_root_subdir/bin/magento indexer:reindex
+$php_executable $magento_root_subdir/bin/magento maintenance:disable
 
 echo '---------------------------------------------------------'
 echo 'set rights'
 
-chmod -R 755 httpdocs/
-chown -R user:group httpdocs/
+#chmod -R 755 $magento_root_subdir/
+chown -R $user:$group $magento_root_subdir
