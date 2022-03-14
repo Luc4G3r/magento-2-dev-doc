14910ec8d15d4851551417c3470a04f6d23db8c2
INblau-LucaGerhardt <95220064+INblau-LucaGerhardt@users.noreply.github.com>
2022-03-14 16:47:33 +0100

    Update full_item_stock_info.sql

--- util/sql_queries/full_item_stock_info.sql
+++ util/sql_queries/full_item_stock_info.sql
@@ -11,40 +11,52 @@ SELECT
 	@org_qty_reserved quantity_reserved_original,
 	csi.qty quantity_stock,
 	(SELECT
+		(-1 * SUM(ir.quantity))
+		FROM inventory_reservation ir
+		WHERE ir.sku = @sku
+	) total_quantity_reserved,
+	(SELECT
 		SUM(ir.quantity)
 		FROM inventory_reservation ir
 		WHERE ir.sku = @sku
-	) quantity_reserved,
+		AND ir.quantity > 0
+	) total_quantity_locked,
+	(SELECT
+		(-1 * SUM(ir.quantity))
+		FROM inventory_reservation ir
+		WHERE ir.sku = @sku
+		AND ir.quantity < 0
+	) total_quantity_released,
 	(SELECT
 		SUM(soi.qty_ordered)
 		FROM sales_order_item soi
 		WHERE soi.product_id = cpe.entity_id
 		AND soi.created_at >= @org_date
-	) quantity_ordered,
+	) total_quantity_ordered,
 	(SELECT
 		SUM(soi.qty_invoiced)
 		FROM sales_order_item soi
 		WHERE soi.product_id = cpe.entity_id
 		AND soi.created_at >= @org_date
-	) quantity_invoiced,
+	) total_quantity_invoiced,
 	(SELECT
 		SUM(soi.qty_refunded)
 		FROM sales_order_item soi
 		WHERE soi.product_id = cpe.entity_id
 		AND soi.created_at >= @org_date
-	) quantity_refunded,
+	) total_quantity_refunded,
 	(SELECT
 		SUM(soi.qty_canceled)
 		FROM sales_order_item soi
 		WHERE soi.product_id = cpe.entity_id
 		AND soi.created_at >= @org_date
-	) quantity_canceled,
+	) total_quantity_canceled,
 	(SELECT
 		SUM(soi.qty_shipped)
 		FROM sales_order_item soi
 		WHERE soi.product_id = cpe.entity_id
 		AND soi.created_at >= @org_date
-	) quantity_shipped
+	) total_quantity_shipped
 FROM catalog_product_entity cpe
 LEFT JOIN cataloginventory_stock_item csi
 	ON csi.product_id = cpe.entity_id
